7404427d0dc78f34f77c3c091d52ba87bb445af2
DiegoAugustoDev <diegoaugustodev12@gmail.com>
2022-02-02 20:37:10 -0300

    atualização no software

--- script.js
+++ script.js
@@ -11,23 +11,23 @@ function calcular(i){
         classificacao = "abaixo do peso"
     }
 
-    else if (imc >18.5 && imc <24.9){
+    else if (imc >=18.5 && imc <=24.9){
         classificacao = "peso ideal"
     }
 
-    else if (imc >25 && imc <29.9 ){
+    else if (imc >=25 && imc <=29.9 ){
         classificacao = "sobrepeso"
     }
 
-    else if (imc >30 && imc <34.9){
+    else if (imc >=30 && imc <=34.9){
         classificacao = "obesidade grau 1"
     }
 
-    else if (imc >35 && imc <39.9){
+    else if (imc >=35 && imc <=39.9){
         classificacao = "obesidade grau 2"
     }
 
-    else if (imc >40){
+    else if (imc >=40){
         classificacao = "obesidade grau 3"
     }
 
@@ -35,5 +35,5 @@ function calcular(i){
 
 
     let h2 = document.getElementById("mostrarResultado")
-    h2.innerText = `${nome},seu imc é ${imc}, você está com ${classificacao}`
+    h2.innerText = `${nome},seu imc é ${imc}. você está com ${classificacao}`
 }
