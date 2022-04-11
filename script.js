2ae7e60d4419927353c7b4ada06fa8a2dcc99dc2
DiegoAugustoDev <98817641+DiegoAugustoDev@users.noreply.github.com>
2022-04-11 17:20:55 -0300

    Atualização do software

--- script.js
+++ script.js
@@ -1,39 +1,43 @@
-function calcular(i){
-    let nome = document.getElementById("nome").value
-    let peso = document.getElementById("peso").value
-    let altura = document.getElementById("altura").value/100
+let btn = document.getElementById("btn")
+let peso = document.getElementById("peso")
+let altura = document.getElementById("altura")
 
-    let imc = (peso /altura**2).toFixed(1)
 
-    let classificacao = ""
+btn.addEventListener("click", function(){
 
-    if (imc <18.5){
-        classificacao = "você está abaixo do peso"
+    if(altura.value != "" && peso.value != ""){
+        let imc = (peso.value/altura.value**2).toFixed(1)
+        let classificacao = ""
+
+
+        if(imc <18.5){
+            classificacao = "você está a baixo do peso"
+        }
+    
+       if(imc >= 18.5 && imc <24.9){
+            classificacao = "você está no peso ideal"
+        }
+    
+        if(imc >=25 && imc <= 29.9){
+            classificacao = "você está com sobrepeso"
+        }
+    
+        if(imc >=30 && imc <= 34.9){
+            classificacao = "você está com obesidade grau 1"
+        }
+    
+        if(imc >35 && imc <= 39.9){
+            classificacao = "você está com obesidade grau 2"
+        }
+    
+      if(imc >=40){
+          classificacao = "você está com obesidade grau 3"
+      }
+
+         let result = document.getElementById("resultado")
+         result.innerHTML = `IMC: ${imc} (${classificacao})`
     }
-
-    else if (imc >=18.5 && imc <=24.9){
-        classificacao = "você está no peso ideal"
+    else{
+        result.innerHTML = "Preencha os campos"
     }
-
-    else if (imc >=25 && imc <=29.9 ){
-        classificacao = " você está com sobrepeso"
-    }
-
-    else if (imc >=30 && imc <=34.9){
-        classificacao = "você está com obesidade grau 1"
-    }
-
-    else if (imc >=35 && imc <=39.9){
-        classificacao = "você está com obesidade grau 2"
-    }
-
-    else if (imc >=40){
-        classificacao = "você está com obesidade grau 3"
-    }
-
-  
-
-
-    let h2 = document.getElementById("mostrarResultado")
-    h2.innerText = `${nome},seu imc é ${imc} . ${classificacao}`
-}
+})  
